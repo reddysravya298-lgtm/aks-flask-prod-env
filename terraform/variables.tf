7e782be1555d7acdb92c9922711abaa1e46dfd5a
reddysravya298-lgtm <reddysravya298@gmail.com>
2025-10-27 18:58:49 +0530

    Create variables.tf
    Terraform variables for RG, location, ACR

---
+++ terraform/variables.tf
@@ -0,0 +1,3 @@
+variable "resource_group_name" { type = string default = "aks-flask-prod-env-rg" }
+variable "location" { type = string default = "East US" }
+variable "acr_name" { type = string default = "aksflaskprodenvacr" }
